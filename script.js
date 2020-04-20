d5e9a96a6bca8f8ce7efa1d0c218499758f1970d
megane <megane.londono@hotmail.com>
2020-04-20 14:41:35 +0200

    initial files created

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export const endpoint = "https://frontendspring20-34e5.restdb.io/rest/trello";
+export const apiKey = "5e957564436377171a0c232c";
+
+window.addEventListener("load", init);
+
+function init() {
+
+}
